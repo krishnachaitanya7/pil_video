5896eaf7e936165bea338d0bf20cbd080804b736
Krishna <chaitanyaradon89@gmail.com>
2021-02-01 20:12:48 -0600

    Add readme for Pip page

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ def parse_requirements(filename):
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 # Tip: Generate requirements using pigar: pigar -p requirements.txt -P pil_video/
 setup(
     name="pil_video",
@@ -14,10 +16,9 @@ setup(
     description="Play a PIL Image list as a video",
     author=" Kodur Krishna Chaitanya",
     author_email="kodur.chaitanya@colorado.edu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/krishnachaitanya7/pil_video",
     install_requires=parse_requirements("requirements.txt"),
     packages=find_packages(exclude=("tests", "docs")),
 )
-
-# Some important notes to keep in mind,
-
